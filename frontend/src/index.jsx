ebc8c6e155200ebfae099cc9a44727237890812e
M.O.K.K.A <marbeloz.studio@gmail.com>
2022-09-11 18:40:25 +0200

    starting the front end

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,17 +1,27 @@
 //Imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import './styles/style.css';
-import { TokenProvider } from './utils/context/index';
-
-//Preparation
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import './styles/Index.css';
+import GlobalStyle from './utils/style/GlobalStyle';
+//import ErrorNotFound from './pages/ErrorNotFound/index';
+import Home from './pages/Home/index';
+//import LogIn from './pages/LogIn/index';
+//import SignUp from './pages/SignUp/index';
+//import Header from './components/Header/index';
+import {} from './utils/context/index';
 
 //Render
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//<React.StrictMode>
 root.render(
-    <TokenProvider>
-        <App />
-    </TokenProvider>
+    <BrowserRouter>
+        <GlobalStyle />
+        {/*<Header />*/}
+        <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            {/*<Route path="/login" element={<LogIn />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+<Route path="*" element={<ErrorNotFound />}></Route>*/}
+        </Routes>
+    </BrowserRouter>
 );
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+//Imports
+import styled from 'styled-components';
+
+const StyledTest = styled.p``;
+
+function Home() {
+    return <StyledTest>Groupomania</StyledTest>;
+}
+
+//Exports
+export default Home;
